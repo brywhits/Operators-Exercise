6425ee73efff4f1f6214bc23d51bb74a0ce56953
bryantwhitsett <bryantwhitsett71@gmail.com>
2025-06-23 05:26:06 -0400

    Learned how to to use operators more and touched up more on calling methods that involve equations and calclations.

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -5,6 +5,30 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+             
+            int a = 17;
+            int b = 4;
+            
+            int sum = a + b;
+            int difference = a - b;
+            int product = a * b;
+            
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}.");
+            
+            
+            Console.WriteLine("What is the area of the circle? ");
+            string userRadius = Console.ReadLine();
+            double radius = double.Parse(userRadius);
+            radius = AreaOfCircle(radius);
+            Console.WriteLine($"The area of the circle is {radius:F2}");
+        }
+
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI * radius * radius;
         }
     }
 }
